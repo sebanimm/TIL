0c0b525deaecb6b1114ac9abec2c4c479bdd3e58
sebanimm <gks32114221@gmail.com>
2022-07-31 20:58:21 +0900

    2022-07-31

--- C/CodeUp/1492.c
+++ C/CodeUp/1492.c
@@ -1,1 +1,20 @@
-. 
+#include <stdio.h>
+
+int main()
+{
+    int a, b[100], sum = 0;
+    scanf("%d", &a);
+
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d", &b[i]);
+        sum += b[i];
+        b[i] = sum;
+    }
+
+    for (int i = 0; i < a; i++)
+    {
+        printf("%d ", b[i]);
+    }
+    return 0;
+}
