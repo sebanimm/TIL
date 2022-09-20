c4a96365056dcace7b68b207fa8e34e2c8a1b51a
sebanimm <102154824+sebanimm@users.noreply.github.com>
2022-09-20 22:33:23 +0900

    2022-09-20

--- C/Baekjoon/1110.c
+++ C/Baekjoon/1110.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-
-    int a, b, c, d = -1, result, input, count = 0;
-
-    scanf("%d", &input);
-    result = input;
-
-    while (d != result)
-    {
-        a = input / 10;
-        b = input % 10;
-        c = (a + b) % 10;
-        d = (b * 10) + c;
-        input = d;
-        count += 1;
-    }
-    
-    printf("%d", count);
+    printf("%d", 5 / 2);
 }
