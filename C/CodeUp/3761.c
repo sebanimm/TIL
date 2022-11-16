dff6edfa7e552e7e6d8020dcdd21e7553826c63d
sebanimm <102154824+sebanimm@users.noreply.github.com>
2022-11-16 11:25:24 +0900

    2022-11-16

---
+++ C/CodeUp/3761.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int i, a, DT[10000001] = {1, 2, 4, 7}; // 다이나믹 프로그래밍
+
+int main()
+{
+  scanf("%d", &a);
+  for (i = 1; i <= a; i++)
+  {
+    DT[i] = (DT[i - 1] + i) % 137;
+  }
+  printf("%d", DT[a]);
+  return 0;
+}
