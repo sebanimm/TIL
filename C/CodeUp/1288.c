07dd9ace745475bce1f4ca2daa34908987886258
sebanimm <102154824+sebanimm@users.noreply.github.com>
2022-11-30 11:27:47 +0900

    2022-11-30

--- C/CodeUp/1288.c
+++ C/CodeUp/1288.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-
-int m[100][100];
-
-int f(int n, int r)
-{
-  if (m[n][r])
-    return m[n][r];
-  if (n == r)
-    return 1;
-  else if (n < r)
-    return 0;
-  else if (r == 1)
-    return n;
-  return m[n][r] = (f(n - 1, r - 1) + f(n - 1, r));
-}
-
+int DT[100][100];
 int main()
 {
-  int n, r;
-  scanf("%d %d", &n, &r);
-  printf("%d", f(n, r));
+  int n, k;
+  scanf("%d %d", &n, &k);
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      if (j == 1)
+        DT[i][j] = i;
+      else if (i == j)
+        DT[i][j] = 1;
+      else
+        DT[i][j] = DT[i - 1][j - 1] + DT[i - 1][j];
+    }
+  }
+  printf("%d", DT[n][k]);
   return 0;
 }
