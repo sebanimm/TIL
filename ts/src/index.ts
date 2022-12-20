9ced6319ba0593a6174cf1c9144b97a3c8cc0442
sebanimm <102154824+sebanimm@users.noreply.github.com>
2022-12-20 20:17:40 +0900

    end of Typescript

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,5 +1,54 @@
-import { init, exit } from "./myPackage";
+import crypto from "crypto";
 
-init({ debug: true, url: "사밀" });
+interface BlockShape {
+	hash: string;
+	prevHash: string;
+	height: number;
+	data: string;
+}
 
-exit(1);
+class Block implements BlockShape {
+	public hash: string;
+	constructor(
+		public prevHash: string,
+		public height: number,
+		public data: string
+	) {
+		this.hash = Block.calculateHash(prevHash, height, data);
+	}
+
+	static calculateHash(prevHash: string, height: number, data: string) {
+		const toHash = `${prevHash}${height}${data}`;
+		return crypto.createHash("sha256").update(toHash).digest("hex");
+	}
+}
+
+class Blockchain {
+	private blocks: Block[];
+	constructor() {
+		this.blocks = [];
+	}
+	private getPrevHash() {
+		if (this.blocks.length === 0) return "";
+		return this.blocks[this.blocks.length - 1].hash;
+	}
+	public addBlock(data: string) {
+		const newBlock = new Block(
+			this.getPrevHash(),
+			this.blocks.length + 1,
+			data
+		);
+		this.blocks.push(newBlock);
+	}
+	public getBlocks() {
+		return [...this.blocks];
+	}
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("fisrt");
+blockchain.addBlock("second");
+blockchain.addBlock("third");
+
+console.log(blockchain.getBlocks());
