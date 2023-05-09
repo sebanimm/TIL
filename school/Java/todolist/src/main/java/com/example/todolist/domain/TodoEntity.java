babd8f63cc4d5d78676375683216e9793f83bec4
sebanimm <102154824+sebanimm@users.noreply.github.com>
2023-05-09 16:59:20 +0900

    20230509 자바

---
+++ school/Java/todolist/src/main/java/com/example/todolist/domain/TodoEntity.java
@@ -0,0 +1,25 @@
+package com.example.todolist.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String content;
+    @Column(nullable = false)
+    private Boolean completed;
+    @Builder
+    public TodoEntity(String content, Boolean completed) {
+        this.content = content;
+        this.completed = completed;
+    }
+}
---
+++ school/Java/todolist/src/test/java/com/example/todolist/repository/TodoRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.example.todolist.repository;
+
+import com.example.todolist.domain.TodoEntity;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TodoRepositoryTest {
+    @Autowired
+    TodoRepository todoRepository;
+
+    @AfterEach
+    public void 초기화() {
+        // DB 초기화
+        todoRepository.deleteAll();
+    }
+
+    @Test
+    public void 값저장(){
+        // 1. 엔티티 하나 만들기
+        TodoEntity entity = TodoEntity.builder().content("a content").completed(false).build();
+        // 2. DB에 저장
+        todoRepository.save(entity);
+        // 3. 확인하기
+        List<TodoEntity> all = todoRepository.findAll();
+        assertEquals(all.get(0).getContent(), "a content");
+    }
+}
