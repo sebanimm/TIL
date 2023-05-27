310fe3173cac84c1409bd39358e152a52c2db7f7
sebanimm <gks32114221@gmail.com>
2023-05-27 19:45:20 +0900

    react-saga

--- redux/counter/src/index.js
+++ redux/counter/src/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import { Provider } from "react-redux";
-import store from "./store.js";
+import { all } from "redux-saga/effects";
+import { counterSaga } from "./store.js";
+import store, { sagaMiddleware } from "./store.js";
+
+function* rootSaga() {
+	yield all([counterSaga()]);
+}
+
+sagaMiddleware.run(counterSaga);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<Provider store={store}>
--- redux/counter/src/store.js
+++ redux/counter/src/store.js
@@ -1,27 +1,34 @@
-import { applyMiddleware, createStore } from "redux";
+import { delay, put, takeEvery, takeLatest } from "redux-saga/effects";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 import { createLogger } from "redux-logger";
-import thunk from "redux-thunk";
 
 const INCREASE = "INCREASE";
 const DECREASE = "DECREASE";
+const INCREASE_ASYNC = "INCREASE_ASYNC";
+const DECREASE_ASYNC = "DECREASE_ASYNC";
 
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
+export const increaseAsync = () => ({ type: INCREASE_ASYNC });
+export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
 
-export const increaseAsync = () => (dispatch) => {
-	setTimeout(() => {
-		dispatch(increase());
-	}, 1000);
-};
-export const decreaseAsync = () => (dispatch) => {
-	setTimeout(() => {
-		dispatch(decrease());
-	}, 1000);
-};
+function* increaseSaga() {
+	yield delay(1000);
+	yield put(increase());
+}
+function* decreaseSaga() {
+	yield delay(1000);
+	yield put(decrease());
+}
 
+export function* counterSaga() {
+	yield takeEvery(INCREASE_ASYNC, increaseSaga);
+	yield takeLatest(DECREASE_ASYNC, decreaseSaga);
+}
 const initialState = 0;
 
-function reducer(state = initialState, action) {
+export function reducer(state = initialState, action) {
 	switch (action.type) {
 		case INCREASE:
 			return state + 1;
@@ -33,7 +40,7 @@ function reducer(state = initialState, action) {
 }
 
 const logger = createLogger();
-
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+export const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware, logger));
 
 export default store;
