f520e604aa4a33344fa38e4ef2a02ce989654c71
sebanimm <gks32114221@gmail.com>
2023-06-07 00:01:26 +0900

    playwright testing

--- testing-libraries/jest/src/sum.test.js
+++ testing-libraries/jest/src/sum.test.js
@@ -6,11 +6,11 @@ test("1 + 2 는 3", () => {
 
 // .test.js파일에서 바로 테스트하기
 
-describe("계산 테스트", () => {
-	const a = 1;
-	const b = 2;
+// describe("계산 테스트", () => {
+// 	const a = 1;
+// 	const b = 2;
 
-	test("a + b 는 3", () => {
-		expect(a + b).toBe(3);
-	});
-});
+// 	test("a + b 는 3", () => {
+// 		expect(a + b).toBe(3);
+// 	});
+// });
