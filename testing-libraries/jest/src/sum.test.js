31e081343d463cb2543e135a7c1709d40767edfc
sebanimm <gks32114221@gmail.com>
2023-06-06 18:57:19 +0900

    jest testing

---
+++ testing-libraries/jest/src/sum.test.js
@@ -0,0 +1,16 @@
+const sum = require("./sum");
+
+test("1 + 2 는 3", () => {
+	expect(sum(1, 2)).toBe(3);
+});
+
+// .test.js파일에서 바로 테스트하기
+
+describe("계산 테스트", () => {
+	const a = 1;
+	const b = 2;
+
+	test("a + b 는 3", () => {
+		expect(a + b).toBe(3);
+	});
+});
