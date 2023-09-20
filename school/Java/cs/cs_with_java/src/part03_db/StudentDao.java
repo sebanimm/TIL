a2c2381aaf30d938f0d49709fe24046a2f0b93f5
sebanimm <gks32114221@gmail.com>
2023-09-20 14:41:18 +0900

    remove, get 추가

--- school/Java/cs/cs_with_java/src/part03_db/StudentDao.java
+++ school/Java/cs/cs_with_java/src/part03_db/StudentDao.java
@@ -1,16 +1,14 @@
 package part03_db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class StudentDao {
     public void add(Student student) throws ClassNotFoundException, SQLException {
         Class.forName("org.h2.Driver");
         Connection con = DriverManager.getConnection(
-                "jdbc:h2:~/part03", "sa",""
+                "jdbc:h2:tcp://localhost/~/part03", "sa",""
         );
+
         String sql = "insert into students values(?,?,?)";
         PreparedStatement pstmt = con.prepareStatement(sql);
         pstmt.setString(1, student.getId());
@@ -18,8 +16,45 @@ public class StudentDao {
         pstmt.setString(3, student.getDepartment());
 
         pstmt.executeUpdate();
-
         pstmt.close();
         con.close();
+    }
+
+    public Student get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection con = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/part03","sa","");
+        String sql = "select * from students where id = ?";
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        pstmt.setString(1, id);
+
+        ResultSet rs = pstmt.executeQuery();
+        rs.next();
+        String studentId = rs.getString(1);
+        String studentName = rs.getString(2);
+        String studentDepartment = rs.getString(2);
+        Student student = new Student(studentId, studentName, studentDepartment);
+
+        rs.close();
+        pstmt.close();
+        con.close();
+
+        return student;
+    }
+
+    public String remove(String id) throws SQLException, ClassNotFoundException {
+        Class.forName("org.h2.Driver");
+        Connection con = DriverManager.getConnection(
+                "jdbc:h2:tcp://localhost/~/part03", "sa",""
+        );
+
+        String sql = "delete from students where id = ?";
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        pstmt.setString(1, id);
+
+        pstmt.executeUpdate();
+        pstmt.close();
+        con.close();
+
+        return id;
     }
 }
--- school/Java/cs/cs_with_java/src/part03_db/TransactionTest.java
+++ school/Java/cs/cs_with_java/src/part03_db/TransactionTest.java
@@ -5,8 +5,14 @@ import java.sql.SQLException;
 public class TransactionTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         StudentDao dao = new StudentDao();
-        Student s1 = new Student("2223", "ㅁㅇ너ㅗ", "test");
+        Student s1 = new Student("2222", "test", "test");
 
         dao.add(s1);
+
+        Student student = dao.get("2222");
+        System.out.println(student.getName());
+
+        String id = dao.remove("2222");
+        System.out.println(id + " is deleted");
     }
 }
