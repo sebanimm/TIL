e054b52ab51bb10752f483195eda0d7f9404111e
sebanimm <gks32114221@gmail.com>
2023-11-14 18:58:24 +0900

    도커로 로컬에 배포 ~~~~

--- tech-stacks/FastAPI/planner/main.py
+++ tech-stacks/FastAPI/planner/main.py
@@ -1,16 +1,26 @@
-import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
-
 from database.connection import Settings
-from routes.events import event_router
+
 from routes.users import user_router
+from routes.events import event_router
+
+import uvicorn
 
 app = FastAPI()
 
 settings = Settings()
 
-# 라우트 등록
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
@@ -27,4 +37,4 @@ async def home():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
