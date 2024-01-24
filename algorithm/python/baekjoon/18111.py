149a904081714599f117f49b7ff7f488f7ca4e1f
sebanimm <gks32114221@gmail.com>
2024-01-24 23:32:32 +0900

    수정

--- algorithm/python/baekjoon/18111.py
+++ algorithm/python/baekjoon/18111.py
@@ -1,22 +1,25 @@
 import sys
 
-N, M, B = map(int, sys.stdin.readline().split())
-ground = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+input = sys.stdin.readline
+
+N, M, B = map(int, input().split())
+ground = [list(map(int, input().split())) for _ in range(N)]
 time = sys.maxsize
 height = 0
 
-for i in range(257):
+for target in range(257):
     max, min = 0, 0
 
     for i in range(N):
         for j in range(M):
-            if ground[i][j] >= i:
-                max += ground[i][j] - i
+            if ground[i][j] >= target:
+                max += ground[i][j] - target
             else:
-                min += i - ground[i][j]
+                min += target - ground[i][j]
 
     if max + B >= min:
         if min + (max * 2) <= time:
             time = min + (max * 2)
-            height = i
+            height = target
+
 print(time, height)
